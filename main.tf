4800268c19346642f92b4cb1f6f39743ce1c8d4a
LokeshV <lokeshviswa428@gmail.com>
2023-03-13 19:37:53 +0530

    updating ./tf-module-app

--- main.tf
+++ main.tf
@@ -44,7 +44,10 @@ resource "aws_iam_policy" "policy" {
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource": "arn:aws:ssm:us-east-1:553708275319:parameter/${var.env}.${var.component}*"
+        "Resource": [
+        "arn:aws:ssm:us-east-1:553708275319:parameter/${var.env}.${var.component}*",
+        "arn:aws:ssm:us-east-1:553708275319:parameter/nexus*"
+          ]
       },
       {
         "Sid" : "VisualEditor1",
